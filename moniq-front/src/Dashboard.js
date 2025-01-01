7edcdb0f1502a12e6c54f345adb214289363c1ba
AyaHA20 <142727266+AyaHA20@users.noreply.github.com>
2024-12-31 18:54:51 -0800

    Réinstallation de Tailwind CSS et réinitialisation du projet

--- moniq-front/src/Dashboard.js
+++ moniq-front/src/Dashboard.js
@@ -4,7 +4,10 @@ import React from 'react';
 function Dashboard() {
   return (
     <div className="Dashboard">
-      
+       <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind CSS est installé !</h1>
+      <p className="text-xl text-gray-700 mt-4">Cela signifie que Tailwind fonctionne correctement dans ton projet.</p>
+    </div>
     </div>
   );
 }
