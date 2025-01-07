8e3f531d760047cac4eb0dd4c8de194938c65187
Hania Ziane <haniaziane30@gmail.com>
2025-01-08 00:24:27 +0100

    Your commit message

--- moniq-front/src/Users.js
+++ moniq-front/src/Users.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React , { useState } from 'react';
 import "./index.css";
 import NavBar from './Components/NavBar';
 import PageHeader from './Components/PageHeader';
 import LittleCard from './Components/littelCard';
- 
-  
+import Boutton from '../src/UsersComponents/Boutton';
+import Search from '../src/UsersComponents/Search';
 
 function Users() {
+ // 1. Ajouter l'état pour la valeur de la barre de recherche
+  const [search, setSearch] = useState('');
+
+ // 2. Fonction pour mettre à jour l'état de la recherche
+  const handleSearchChange = (e) => {
+     setSearch(e.target.value);
+  };
+
   return (
     <div className="Dashboard bg-lightGray h-screen w-full flex">
       {/* NavBar à gauche */}
@@ -14,12 +22,12 @@ function Users() {
       
       {/* Contenu principal à droite */}
       <div className="bg-gray-100 w-5/6 h-full flex flex-col px-6 py-2">
-     
+        
         {/* Conteneur pour le PageHeader et les LittleCards */}
         <div className="flex justify-between items-start w-full">
           {/* PageHeader */}
           <div className="page-header-container mt-0">
-            <PageHeader pageName="Dashboard" />
+            <PageHeader pageName="Users" />
           </div>
           
           {/* Conteneur pour les cartes */}
@@ -28,6 +36,22 @@ function Users() {
             <LittleCard title="Errors/Alerts" apiEndpoint="https://your-backend-api.com/orders-count" />
             <LittleCard title="Users" apiEndpoint="https://your-backend-api.com/products-count" />
           </div>
+        </div>
+
+
+          {/* 3. Ajouter le composant Search */}
+          <div className="my-4">
+          <Search
+            placeholder="Search for users..."
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </div>
+
+         {/* Ajouter le composant Button */}
+         <div className="my-4">
+          <Boutton label="Add Member" color="blue" /> {/* Bouton bleu */}
+          <Boutton label="Edit Role" color="white" /> {/* Bouton blanc */}
         </div>
 
         
